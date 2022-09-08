addabdfa666c23e9ea0138ab23fcfb52fa3312b6
Giuliana Cabral <cabral.giu@hotmail.com>
2022-09-08 18:09:32 -0300

    Navbar adicionada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom";
 import {Home} from "./pages/Home"
+import {Navbar} from "./pages/components/Navbar"
 
 
 function App() {
   return <>
+  <Navbar>
   <Routes>
     <Route path="/" element={<Home />} />
   </Routes>
+  </Navbar>
   
   </>
 }

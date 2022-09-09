790da3dede4c2eef1e7c1b901a03309b80e23cf2
Giuliana Cabral <cabral.giu@hotmail.com>
2022-09-09 15:23:58 -0300

    Iniciando CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import {Home} from "./pages/Home"
 import {Navbar} from "./pages/components/Navbar"
 import {AddItems} from './pages/AddItems';
+import "./style.css";
 
 function App() {
   return <>
@@ -11,6 +12,7 @@ function App() {
     <Route path="/add-items" element={<AddItems />} />
   </Routes>
   </Navbar>
+
   
   </>
 }
